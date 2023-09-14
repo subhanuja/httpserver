a3ccba9b553af25d2bf3b2645a849521bd0559e4
subhanuja <gangisettysudheerkumar@gmail.com>
2023-09-13 21:58:21 -0700

    reactb

---
+++ reactproject/contact-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App';
+
+ 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
+
+
---
+++ shopping/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>sudheer</h1>
+    </div>
+  )
+}
+
+export default App
+
